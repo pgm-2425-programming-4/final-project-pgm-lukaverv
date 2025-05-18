94e5a7ba57b1f4e07afb5c70f2545f55cc63fd1f
pgm-lukaverv <lukas.vervaet@student.arteveldehs.be>
2025-05-18 20:30:37 +0200

    added pagination & backlog with hardcoded pageSize

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
-import Counter from './Counter'
+import Backlog from "./components/PaginatedBacklog/Backlog"; 
+import "./styles/app.css";
+
+export const API_TOKEN = "5c4e2ed859f93e6657e9f7bd7879d1daadfc3c9ddcc352ca62e1a12b921bb0cb8b8d6f13f088db60fd21de701a6e32cfcc3f9eb0310c350ecaabe40c90e5b67d0d00c64350b956f6d249275a541da4916bce447ef9c7cdd04f1a076303242104f6fce671e3273e1949a75c41c8d0a5fce036c3466497daec06705c28fa9f4093"; 
 
 function App() {
     return (
         <>
-        
+            <Backlog />
         </>
-    )
+    );
 }
 
-export default App
+export default App;
---
+++ client/src/components/PaginatedBacklog/Backlog.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { API_TOKEN } from "../../App";
+import Pagination from "./Pagination";
+
+function Backlog() {
+    const [tasks, setTasks] = useState([]);
+    const [pagination, setPagination] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const limit = 5;
+    const start = (currentPage - 1) * limit;
+
+    useEffect(() => {
+        fetch(
+            `http://localhost:1337/api/tasks?filters[task_status][title][$eq]=Backlog&pagination[start]=${start}&pagination[limit]=${limit}&populate=*`,
+            {
+                headers: {
+                    Authorization: `Bearer ${API_TOKEN}`,
+                },
+            }
+        )
+            .then((res) => res.json())
+            .then((json) => {
+                setTasks(json.data);
+                setPagination(json.meta.pagination);
+            });
+    }, [start]);
+
+    function handlePageChange(page) {
+        setCurrentPage(page);
+    }
+
+    return (
+        <div>
+            <h2>Backlog Taken</h2>
+            <ul>
+                {tasks.map((task) => (
+                    <li key={task.id}>{task.title}</li>
+                ))}
+            </ul>
+
+            {pagination && (
+                <Pagination
+                    totalPages={Math.ceil(pagination.total / limit)}
+                    currentPage={currentPage}
+                    onPageChanged={handlePageChange}
+                />
+            )}
+        </div>
+    );
+}
+
+export default Backlog;
