0317724d1b79854c183b41ce31c3e584869d7947
pgm-lukaverv <lukas.vervaet@student.arteveldehs.be>
2025-05-26 15:31:21 +0200

    added Ready_For_Review component to display tasks with "ready for review" status

--- client/src/components/Tasks/TaskList.jsx
+++ client/src/components/Tasks/TaskList.jsx
@@ -1,4 +1,5 @@
 import In_Progress from "./TaskStatus/In_Progress";
+import Ready_For_Review from "./TaskStatus/Ready_For_Review";
 import To_Do from "./TaskStatus/To_Do";
 
 function TaskList() {
@@ -7,6 +8,7 @@ function TaskList() {
             <section className="main__task-status">
                 <To_Do />
                 <In_Progress />
+                <Ready_For_Review />
             </section>
         </>
     );
---
+++ client/src/components/Tasks/TaskStatus/Ready_For_Review.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { API_TOKEN } from "../../../App";
+
+function Ready_For_Review() {
+    const [tasks, setTasks] = useState([]);
+    
+    useEffect(() => {
+        fetch("http://localhost:1337/api/tasks?filters[task_status][title][$eq]=ready_for_review&populate=*", {
+            headers: {
+                Authorization: `Bearer ${API_TOKEN}`,
+            },
+        })
+        .then((res) => res.json())
+        .then((json) => {
+            setTasks(json.data);
+        });
+    }, [])
+    return (
+        <>
+            <article className="task-status task-status--ready-for-review">
+                <h3 className="task-status__title">Ready for review</h3>
+                <div className="task-status__tasks">
+                    {tasks.map((task) => (
+                        <div className="task" key={task.id}>
+                            <p className="task__title">{task.title}</p>
+                            <div className="task__labels">
+                                {task.labels.map((label) => (
+                                    <span className="task__label" key={label.id}>
+                                        {label.title}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+        </article>
+        </>
+    );
+}
+
+export default Ready_For_Review;
