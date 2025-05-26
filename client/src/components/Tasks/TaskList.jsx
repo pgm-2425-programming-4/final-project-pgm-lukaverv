1cd1b39f54b03c7be1d2517c0924f02060f48422
pgm-lukaverv <lukas.vervaet@student.arteveldehs.be>
2025-05-26 15:29:37 +0200

    added To_Do component with fetched tasks inside that has the "to-do" status

--- client/src/components/Tasks/TaskList.jsx
+++ client/src/components/Tasks/TaskList.jsx
@@ -1,7 +1,10 @@
+import To_Do from "./TaskStatus/To_Do";
+
 function TaskList() {
     return (
         <>
             <section className="main__task-status">
+                <To_Do />
             </section>
         </>
     );
---
+++ client/src/components/Tasks/TaskStatus/To_Do.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { API_TOKEN } from "../../../App";
+
+function To_Do() {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:1337/api/tasks?filters[task_status][title][$eq]=to_do&populate=*", {
+            headers: {
+                Authorization: `Bearer ${API_TOKEN}`,
+            },
+        })
+        .then((res) => res.json())
+        .then((json) => {
+            setTasks(json.data);
+        });
+    }, [])
+    return (
+        <>
+            <article className="task-status task-status--to-do">
+                <h3 className="task-status__title">To do</h3>
+                <div className="task-status__tasks">
+                    {tasks.map((task) => (
+                        <div className="task" key={task.id}>
+                            <p className="task__title">{task.title}</p>
+                            <div className="task__labels">
+                                {task.labels.map((label) => (
+                                    <span className="task__label" key={label.id}>
+                                        {label.title}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </article>
+        </>
+    );
+}
+
+export default To_Do;
