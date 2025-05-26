cf728e22251d92b289c4ba86e4e787fe5784dcc8
pgm-lukaverv <lukas.vervaet@student.arteveldehs.be>
2025-05-26 17:00:03 +0200

    merged all branches to main and commited all changes to main to start testing online deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
-import "./App.css"
+import NavBar from "./components/Navigationbar/NavBar";
 import ProjectList from "./components/Projects/ProjectList";
+import TaskList from "./components/Tasks/TaskList";
+import "./App.css"
 
-export const API_TOKEN = "5c4e2ed859f93e6657e9f7bd7879d1daadfc3c9ddcc352ca62e1a12b921bb0cb8b8d6f13f088db60fd21de701a6e32cfcc3f9eb0310c350ecaabe40c90e5b67d0d00c64350b956f6d249275a541da4916bce447ef9c7cdd04f1a076303242104f6fce671e3273e1949a75c41c8d0a5fce036c3466497daec06705c28fa9f4093"; 
+export const API_TOKEN = "b5abb911ff97819bc8afb63b49ef6c1c8a63e9d58c1e9c79cc9ff6de5badf73365d083bf7b1b340cd5c6d7e58019fb98d945e0fb697c0c7ab95ce40875152645deabecacaf16a763c4db4595d02c0f5c4e2984fbe281a6b4d36443223a7f6b2a7050ed76f4b2f463c6e7cab7df9f0f9bb12afed6da67947230d6e63d4a1bb5fa"
 
 function App() {
     return (
         <>
-          <ProjectList />
+            <ProjectList />
+            <main className="main">
+                <NavBar/>
+                <TaskList />
+            </main>
         </>
     );
 }
