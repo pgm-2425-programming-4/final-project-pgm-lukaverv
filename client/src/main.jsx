533caf40fce22b2ee11b99f97c588e518962aaf0
pgm-lukaverv <lukas.vervaet@student.arteveldehs.be>
2025-06-10 16:40:11 +0200

    fix: correct import path for router in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "@tanstack/react-router";
 import "./index.css";
-import router from "./router";
+import router from "./Router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={new QueryClient()}>
       <RouterProvider router={router} />
     </QueryClientProvider>
-  </StrictMode>
+  </StrictMode>,
 );
